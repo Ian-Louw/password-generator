ff9113a06e675ce0e108012386c40c7cf962ffe7
Ian Louw <ianlouw.dev@gmail.com>
2026-02-08 00:03:10 +0100

    Fix bcrypt ValueError for passwords exceeding 72 bytes

--- hasher.py
+++ hasher.py
@@ -3,5 +3,5 @@ import bcrypt
 
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt(rounds=12)
-    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    hashed = bcrypt.hashpw(password.encode("utf-8")[:72], salt)
     return hashed.decode("utf-8")
